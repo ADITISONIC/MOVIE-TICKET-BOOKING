0f9c984835331565d977546b3d444033952420dd
iaditi <aditipython@gmail.com>
2025-03-04 19:06:23 +0530

    changes updated

--- movie-ticket-frontend/src/api.js
+++ movie-ticket-frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api"; // Replace with your backend URL
+const API_URL = "http://localhost:3000/api"; // Replace with your backend URL
 
 // Register a new user
 export const register = async (email, password, role) => {
